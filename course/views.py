18ace2a1f84c7cd130b576a06033f4895ea4e707
unknown <baraa.fayezz@gmail.com>
2022-08-03 20:37:33 +0200

    Category Model + View + Serializer

--- course/views.py
+++ course/views.py
@@ -26,7 +26,7 @@ def Category_List(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
