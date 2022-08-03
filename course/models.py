12c577226129230d8ff142c88cc6a84fdeb3b0a7
unknown <baraa.fayezz@gmail.com>
2022-08-03 17:01:04 +0200

    Category Model

--- course/models.py
+++ course/models.py
@@ -1,19 +1,11 @@
 from django.db import models
 
-# Create your models here.
-# Category -- Student_Course (M2M)
 
+# Category Model
 
 class Category (models.Model):
-    cat_name = models.CharField(max_length=50)
-    courses = models.ManyToManyField(Course)
+    track = models.CharField(max_length=50)
+    courses = models.ForeignKey(Course)
 
     def __str__(self):
-        return self.cat_name
-
-
-class Student_Course(models.Model):
-    student = models.ForeignKey(
-        Student, related_name='student_course', on_delete=models.CASCADE)
-    course = models.ForeignKey(
-        Course, related_name='student_course', on_delete=models.CASCADE)
+        return self.track
