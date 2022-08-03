04187931cd191f20e153401dedd770a3a842532d
Shimaa Attallah <shimaamattallah@gmail.com>
2022-08-03 18:46:36 +0200

    add api

--- elearning/settings.py
+++ elearning/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'course',
+    'review.apps.ReviewConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'elearning.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangotask2',
+        'USER': 'iti',
+        'PASSWORD': '000',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
