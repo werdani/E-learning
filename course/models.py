8991f87ae3b86e1af2abaf9e08c407caa45fd2a2
unknown <baraa.fayezz@gmail.com>
2022-08-03 03:48:21 +0200

    Categroty & student_course models

--- course/models.py
+++ course/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+# Category -- Student_Course (M2M)
+
+
+class Category (models.Model):
+    cat_name = models.CharField(max_length=50)
+    courses = models.ManyToManyField(Course)
+
+    def __str__(self):
+        return self.cat_name
+
+
+class Student_Course(models.Model):
+    student = models.ForeignKey(
+        Student, related_name='student_course', on_delete=models.CASCADE)
+    course = models.ForeignKey(
+        Course, related_name='student_course', on_delete=models.CASCADE)
