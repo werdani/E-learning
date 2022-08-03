fc531e068e73a953ebe5da9b8d8f839bf3ff28ab
unknown <baraa.fayezz@gmail.com>
2022-08-03 19:15:40 +0200

    Category Model + View + Serializer

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+
+# Category Model Registered
+admin.site.register(Category)
--- elearning/urls.py
+++ elearning/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import views
 from django.contrib import admin
 from django.urls import path
-
+from course import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('category/', views.Category_List),
+    path('category/<int:pk>', views.Category_pk),
 ]
