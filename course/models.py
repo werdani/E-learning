6772b32a05e6a8ba2f31f5c1d47abf6d45f22866
alaa-abouzied <alaa.m.abouzied@gmail.com>
2022-08-06 20:44:18 +0200

    edit course moddel

--- course/models.py
+++ course/models.py
@@ -13,11 +13,13 @@ class Course(models.Model):
     course_rate=models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
     #course_category=models.ForeignKey(Category, on_delete=models.CASCADE, related_name="track_courses" )
     course_created_at=models.DateTimeField(auto_now_add=True)
+    student_course_name = models.ManyToManyField(User,related_name="student_course")
+    course_instructor_name = models.ForeignKey(User,on_delete=models.CASCADE,related_name="instructor_course")
+    
     
     def __str__(self):
         return self.course_name
     
-class Student_Course(models.Model):
-    student_course_name = models.ManyToManyField(Course,related_name="student_course")
-    user = models.ForeignKey(
-        User, on_delete=models.PROTECT, related_name='userCourse')
+
+
+  
--- elearning/settings.py
+++ elearning/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     
 ]
 REST_FRAMEWORK = {
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    #  'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     # 'DEFAULT_AUTHENTICATION_CLASSES':
     # ['rest_framework.authentication.TokenAuthentication'],
     # 'DEFAULT_PERMISSION_CLASSES':
